06aada019e54dba504af8d5d89101091c4ad359d
Muhammad Muaz <21100231@lums.edu.pk>
2023-04-10 08:57:42 -0500

    Added trouble and todo-comments nvim plugins

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -56,13 +56,24 @@ return packer.startup(function(use)
   use { "lukas-reineke/indent-blankline.nvim", commit = "db7cbcb40cc00fc5d6074d7569fb37197705e7f6" }
   use { "goolord/alpha-nvim", commit = "0bb6fc0646bcd1cdb4639737a1cee8d6e08bcc31" }
 	use {"folke/which-key.nvim"}
-
   use {"tpope/vim-surround"}
+
 	-- Colorschemes
   use { "folke/tokyonight.nvim", commit = "66bfc2e8f754869c7b651f3f47a2ee56ae557764" }
   use { "lunarvim/darkplus.nvim", commit = "13ef9daad28d3cf6c5e793acfc16ddbf456e1c83" }
   use{  "rebelot/kanagawa.nvim" }
 
+  -- Trouble.nvim
+use {
+  "folke/trouble.nvim",
+  requires = "nvim-tree/nvim-web-devicons",
+}
+
+  -- TODO.nvim
+use {
+  "folke/todo-comments.nvim",
+  requires = "nvim-lua/plenary.nvim"
+  }
 	-- Cmp 
   use { "hrsh7th/nvim-cmp", commit = "b0dff0ec4f2748626aae13f011d1a47071fe9abc" } -- The completion plugin
   use { "hrsh7th/cmp-buffer", commit = "3022dbc9166796b644a841a02de8dd1cc1d311fa" } -- buffer completions
