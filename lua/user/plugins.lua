64364f999d58f193def264bf488d949353336234
Muhammad Muaz <21100231@lums.edu.pk>
2023-06-30 19:14:26 +0500

    Added notify and eyeliner plugins

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -47,7 +47,13 @@ return packer.startup(function(use)
   use { "JoosepAlviste/nvim-ts-context-commentstring", commit = "4d3a68c41a53add8804f471fcc49bb398fe8de08" }
   -- use { "kyazdani42/nvim-web-devicons", commit = "563f3635c2d8a7be7933b9e547f7c178ba0d4352" }
   use{'nvim-tree/nvim-web-devicons'}
-  use { "kyazdani42/nvim-tree.lua", commit = "7282f7de8aedf861fe0162a559fc2b214383c51c" }
+  use {
+    'nvim-tree/nvim-tree.lua',
+    requires = {
+      'nvim-tree/nvim-web-devicons', -- optional
+    },
+  }
+  -- use { "kyazdani42/nvim-tree.lua", commit = "7282f7de8aedf861fe0162a559fc2b214383c51c" }
   use { "akinsho/bufferline.nvim", commit = "83bf4dc7bff642e145c8b4547aa596803a8b4dc4" }
 	use { "moll/vim-bbye", commit = "25ef93ac5a87526111f43e5110675032dbcacf56" }
   use { "nvim-lualine/lualine.nvim", commit = "a52f078026b27694d2290e34efa61a6e4a690621" }
@@ -146,6 +152,19 @@ use{
   -- Vim Tex plugin
   use {'lervag/vimtex'}
 
+  -- Vim notify
+  use {'rcarriga/nvim-notify'}
+
+  -- Eyeline 
+  use {
+    'jinh0/eyeliner.nvim',
+    config = function()
+      require'eyeliner'.setup {
+        highlight_on_key = true, -- show highlights only after keypress
+        dim = true              -- dim all other characters if set to true (recommended!)
+      }
+    end
+  }
   -- TODO.nvim
 use {
   "folke/todo-comments.nvim",
